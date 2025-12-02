3ae050e7a6780788cf73fb72f7ea7d7b464e3e80
agdoran <15125129+agdoran@users.noreply.github.com>
2025-12-02 23:05:44 +0000

    Update rg_checker_v.0.0.2.pl

--- rg_checker_v.0.0.1.pl
+++ rg_checker_v.0.0.1.pl
@@ -11,9 +11,16 @@ my $mulrg = 0;
 my $notdef = 0;
 my $isdef = 0;
 
+my $mm = 0;
+my $ml = 0;
+
 my %colcount;
 my %RGcount;
 my %RGheadcount;
+my %RGMM;
+my %RGML;
+
+my $totalrecords = 0;
 
 # this will read from standard in (the BAM must be piped to this script)
 while(my $line = <>){
@@ -37,6 +44,14 @@ while(my $line = <>){
 	# skip header after parsing RG rows
 	next if($line =~ m/^\@/);
 
+	# track total count of records
+	$totalrecords++;
+
+	# track per line mm and ml - not a counter. 0 not present, 1 and MM+ML are present
+	my $lineMM = 0;
+	my $lineML = 0;
+	my $currentRG = "";
+
 	my @cols = split("\t", $line);
 	$colcount{@cols}++; # count number of columns in row
 
@@ -49,10 +64,12 @@ while(my $line = <>){
 
 			# count/increment the number of occurrances of the read RG 
 			my $rowrg = (split(/\:/, $cols[$i]))[2];
+			$currentRG = (split(/\:/, $cols[$i]))[2];
 
 			# check to ensure row RG tag is not null 
 			unless(defined $rowrg){
 				$rowrg = "ISNULL";
+				$currentRG = "ISNULL";
 			}
 
 			# check if the read RG is already defined (should be in the header)
@@ -66,6 +83,16 @@ while(my $line = <>){
 			$RGcount{$rowrg}++;
 
 		}
+
+		# count the rows that have MM and ML tags (no check for defined value)
+		if($cols[$i] =~ m/^MM/i){
+			$mm++;
+			$lineMM = 1;
+		}
+		if($cols[$i] =~ m/^ML/i){
+			$ml++;
+			$lineML = 1;
+		}
 	}
 
 	# classify the presence of RG tags (1,0 or >1 RG tags in row)
@@ -73,21 +100,56 @@ while(my $line = <>){
 		$rg++;
 	}elsif($rowc == 0){
 		$norg++;
+		
+		# RG value is missing from row - different from a RG present with no value
+		my $miss = "MISS";
+		$RGheadcount{$miss} = 0;
+		$RGcount{$miss}++;
+
+		$currentRG = "MISS";
+
 	}elsif($rowc > 1){
 		$mulrg++;
-	}	
+		my $many = "MANY";
+		$RGheadcount{$many} = 0;
+		$RGcount{$many}++;
+
+		$currentRG = "MANY";
+	}
+
+
+	# increment the count for MM and ML for the current read group
+	if(exists $RGMM{$currentRG}){
+		$RGMM{$currentRG}++ if($lineMM > 0);
+	}else{
+		if($lineMM > 0){
+			$RGMM{$currentRG} = 1;
+		}else{
+			$RGMM{$currentRG} = 0;
+		}
+	}
+
+	if(exists $RGML{$currentRG}){
+		$RGML{$currentRG}++ if($lineML > 0);
+	}else{
+		if($lineML > 0){
+			$RGML{$currentRG} = 1;
+		}else{
+			$RGML{$currentRG} = 0;
+		}
+	}
 }
 
 #print header
-print "#OneRG-in-row\tNoRG-in-row\tManyRG-in-row\tRGdefined-in-header\tRGnotDefined-in-header\n";
+print "#Total-records\tOneRG-in-row\tNoRG-in-row\tManyRG-in-row\tRGdefined-in-header\tRGnotDefined-in-header\tRows-with-MM\tRows-with-ML\n";
 # print metrics
-print "$rg\t$norg\t$mulrg\t$isdef\t$notdef\n";
+print "$totalrecords\t$rg\t$norg\t$mulrg\t$isdef\t$notdef\t$mm\t$ml\n";
 
 # print a count of the number rows with X number of columns
-print "#RD_ID\tHeader-count\tCount-of-rows\n";
+print "#RG_ID\tHeader-count\tCount-of-rows\tRecords-with-MM\tRecords-with-ML\n";
 foreach my $key (keys %RGcount){
 
-	print "##$key\t$RGheadcount{$key}\t$RGcount{$key}\n";
+	print "##$key\t$RGheadcount{$key}\t$RGcount{$key}\t$RGMM{$key}\t$RGML{$key}\n";
 
 }
 
